0a334ebc92618145ea36458a6e526fc6f1a38680
Afonsoafonso413 <afonso.apar@gmail.com>
2026-03-29 23:19:52 +0100

    rele

--- src/domuscontrol/model/dispositivos/Rele.java
+++ src/domuscontrol/model/dispositivos/Rele.java
@@ -1,1 +1,35 @@
 package domuscontrol.model.dispositivos;
+
+/**
+ * Relé - o dispositivo mais simples. Apenas liga ou desliga.
+ */
+public class Rele extends Dispositivo {
+
+    public Rele() {
+        super();
+    }
+
+    public Rele(String id, String marca, String modelo, double consumoWh) {
+        super(id, marca, modelo, consumoWh);
+    }
+
+    public Rele(Rele outro) {
+        super(outro);
+    }
+
+   @Override
+public double getConsumoAtual() {
+    if (!isLigado()) return 0.0;
+    return getConsumoWh();
+}
+
+    @Override
+    public String estadoEspecifico() {
+        return "Relé simples";
+    }
+
+    @Override
+    public Dispositivo clone() {
+        return new Rele(this);
+    }
+}
